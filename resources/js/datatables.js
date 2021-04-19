d62b126bc3d5113572bebe17f298d0aa1ce7dfbd
Victor Allen <vamuigua@gmail.com>
2021-04-19 19:21:48 +0300

    feat(Datatables): add export to excel
    All tables using datatables now have the ability to export data to excel through Jszip package

--- resources/js/datatables.js
+++ resources/js/datatables.js
@@ -3,6 +3,44 @@ jQuery(function() {
         responsive: true,
         fixedHeader: true,
         dom: "Bfrtip",
-        buttons: ["pdf", "print", "colvis"]
+        lengthMenu: [
+            [10, 25, 50, -1],
+            ["10 rows", "25 rows", "50 rows", "Show all"]
+        ],
+        buttons: [
+            {
+                extend: "pdf",
+                text:
+                    '<i class="fas fa-file-pdf fa-1x" aria-hidden="true"> Export to PDF</i>'
+            },
+            {
+                extend: "csv",
+                text: '<i class="fas fa-file-csv fa-1x"> Export to CSV</i>',
+                exportOptions: {
+                    modifier: {
+                        page: "current"
+                    }
+                }
+            },
+            {
+                extend: "excel",
+                text: '<i class="fas fa-file-excel fa-1x"> Export to excel</i>',
+                exportOptions: {
+                    modifier: {
+                        page: "current"
+                    }
+                }
+            },
+            {
+                extend: "print",
+                text: '<i class="fas fa-print fa-1x"> Print current page</i>',
+                exportOptions: {
+                    modifier: {
+                        page: "current"
+                    }
+                }
+            },
+            "pageLength"
+        ]
     });
 });
