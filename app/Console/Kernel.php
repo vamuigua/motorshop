53283892ceeda52fbb977dfa784d01e485b17b43
Victor Allen <vamuigua@gmail.com>
2022-04-20 05:47:43 +0000

    update(production): updated production files

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call(new DeleteTempImages)->everyMinute();
-        // $schedule->call(new DeleteTempImages)->daily()->withoutOverlapping();
+        // $schedule->call(new DeleteTempImages)->everyMinute();
+        $schedule->call(new DeleteTempImages)->daily()->withoutOverlapping();
     }
 
     /**
